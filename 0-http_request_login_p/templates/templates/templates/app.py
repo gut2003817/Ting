3e7acf64a92aa7947ceb26745d73c84d23ad2d63
gut2003817 <132485552+gut2003817@users.noreply.github.com>
2023-05-18 14:09:45 +0800

    update

--- 0-http_request_login_p/templates/templates/templates/app.py
+++ 0-http_request_login_p/templates/templates/templates/app.py
@@ -4,6 +4,30 @@ app = Flask(__name__)
 from flask import request
 from flask import render_template
 
+users = {
+    'user1': {
+        'name': 'User One',
+        'email': 'user1@example.com'
+    },
+    'user2': {
+        'name': 'User Two',
+        'email': 'user2@example.com'
+    },
+    'user3': {
+        'name': 'User Three',
+        'email': 'user3@example.com'
+    },
+    'user4': {
+        'name': 'User Four',
+        'email': 'user4@example.com'
+    },
+    'user5': {
+        'name': 'User Five',
+        'email': 'user5@example.com'
+    },
+}
+
+
 # 驗證登入函式
 def valid_login(user, passwd):
     # 檢查使用者名稱和密碼是否有效
